33e2d61529d962bf2802baacc55831c60065f85e
Josh Justice <josh@need-bee.com>
2014-11-25 15:52:11 -0500

    adds service times

--- webroot/index.php
+++ webroot/index.php
@@ -5,5 +5,13 @@
 <body>
 <h1>Hello, world! We're glad you're here.</h1>
 <h2>11/25</h2>
+
+<h2>Service Times</h2>
+<ul>
+    <?php foreach( ['9:00 a.m.', '11:00 a.m.', '1:00 p.m'] as $time ): ?>
+        <li><?php echo $time ?></li>
+    <?php endforeach ?>
+</ul>
+
 </body>
 </html>
